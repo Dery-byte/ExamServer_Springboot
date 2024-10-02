9a41f7a9935dfe54ed673180c403397881a096a0
unknown <emmanueldery31@gmail.com>
2024-10-02 19:26:43 +0000

    Dockerfile

--- src/main/java/com/exam/controller/TheoryController.java
+++ src/main/java/com/exam/controller/TheoryController.java
@@ -29,41 +29,39 @@ public class TheoryController {
 
 
     @PostMapping("theoryquestion/add")
-    public ResponseEntity<TheoryQuestions> add(@RequestBody TheoryQuestions theoryQuestions){
+    public ResponseEntity<TheoryQuestions> add(@RequestBody TheoryQuestions theoryQuestions) {
         return ResponseEntity.ok(this.theoryService.addQuestions(theoryQuestions));
     }
 
     @GetMapping("theoryquestion/{quesId}")
-    public  TheoryQuestions getSpecificQuestionsOfQuizAdmin(@PathVariable("quesId") Long quesId ){
+    public TheoryQuestions getSpecificQuestionsOfQuizAdmin(@PathVariable("quesId") Long quesId) {
         return this.theoryService.getQuestions(quesId);
 //        return ResponseEntity.ok(list);
     }
 
     //Delete Question
     @DeleteMapping("/theoryquestion/{quesId}")
-    public void deleteQuestion(@PathVariable("quesId") Long quesId){
+    public void deleteQuestion(@PathVariable("quesId") Long quesId) {
         this.theoryService.deleteQuestion(quesId);
     }
 
     //update Questions
     @PutMapping("/theoryquestion/updateQuestions")
-    public TheoryQuestions updateQuestion(@RequestBody TheoryQuestions theoryQuestions){
-        return  this.theoryService.updateQuestions(theoryQuestions);
+    public TheoryQuestions updateQuestion(@RequestBody TheoryQuestions theoryQuestions) {
+        return this.theoryService.updateQuestions(theoryQuestions);
     }
 
 
     @GetMapping("theoryquestion/quiz/all/{qid}")
-    public  ResponseEntity<?> getQuestionsOfQuizAdmin(@PathVariable("qid") Long qid){
+    public ResponseEntity<?> getQuestionsOfQuizAdmin(@PathVariable("qid") Long qid) {
         Quiz quiz = new Quiz();
         quiz.setqId(qid);
-        Set<TheoryQuestions> questionsOfQuiz =this.theoryService.getQuestionsForSpecificQuiz(quiz);
+        Set<TheoryQuestions> questionsOfQuiz = this.theoryService.getQuestionsForSpecificQuiz(quiz);
         List<TheoryQuestions> list = new ArrayList<>(questionsOfQuiz);
 //        Collections.shuffle(list);
 //        return ResponseEntity.ok(questionsOfQuiz);
         return ResponseEntity.ok(list);
     }
-
-
 
 
     @PostMapping("/theoryupload/{quizId}")
@@ -77,18 +75,21 @@ public class TheoryController {
             int numberOfQuestions = Integer.parseInt(quiz.getNumberOfQuestions());
             // Set the quiz for each question
             theoryQuestions.forEach(question -> question.setQuiz(quiz));
-            if(theoryQuestions.size()<= numberOfQuestions)
-            {
-                // Save questions
-                List<TheoryQuestions> savedQuestions = theoryService.saveAllQuestions(theoryQuestions);
-                return new ResponseEntity<>("Uploaded " + savedQuestions.size() + " questions to the quiz with ID: " + quizId, HttpStatus.CREATED);
-            }
-            else{
-                return ResponseEntity.status( HttpStatus.BAD_REQUEST).body("Number of questions should be " + numberOfQuestions+ " but you provided " + theoryQuestions.size());
-            }
-        } else {
-            return new ResponseEntity<>("Quiz with ID " + quizId +  " not found.", HttpStatus.NOT_FOUND);
-        }
-    }
 
+
+//            if(theoryQuestions.size()<= numberOfQuestions)
+//            {
+            // Save questions
+            List<TheoryQuestions> savedQuestions = theoryService.saveAllQuestions(theoryQuestions);
+//                return new ResponseEntity<>("Uploaded " + savedQuestions.size() + " questions to the quiz with ID: " + quizId, HttpStatus.CREATED);
+//            }
+//            else{
+//                return ResponseEntity.status( HttpStatus.BAD_REQUEST).body("Number of questions should be " + numberOfQuestions+ " but you provided " + theoryQuestions.size());
+//            }
+//        } else {
+//            return new ResponseEntity<>("Quiz with ID " + quizId +  " not found.", HttpStatus.NOT_FOUND);
+//        }
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body("Questions saved successfully");
+    }
 }
