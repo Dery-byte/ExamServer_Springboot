bb63a778cce14a2c9e8e2c9bba91445452c6f5ea
dery-byte <emmanueldery31@gmail.com>
2026-01-22 22:07:40 +0000

    Committing Database connections

--- src/main/java/com/exam/controller/AuthenticationController.java
+++ src/main/java/com/exam/controller/AuthenticationController.java
@@ -38,7 +38,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = "*")
+//@CrossOrigin(origins = "*")
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
